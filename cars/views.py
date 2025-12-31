f4dd090e5897a25ddfe740ee70db2efc2bf19d81
Camila Marques <milamazim@gmail.com>
2025-12-31 09:46:24 -0300

    modified cars template and added a search feature

--- cars/views.py
+++ cars/views.py
@@ -1,6 +1,11 @@
 from django.shortcuts import render
 from cars.models import Car
 
-def cars_view(request):
-    cars = Car.objects.all() # select * from cars
+def cars_view(request):    
+    cars = Car.objects.all().order_by('model') # select * from cars order by model
+    search = request.GET.get('search') # filtro da busca do usuario
+
+    if search:
+        cars = Car.objects.filter(model__icontains=search).order_by('model')
+
     return render(request, 'cars.html', {'cars':cars})
