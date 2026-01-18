70152fb272db3c8341b7ad9f541b5722d6f1aa56
Camila Marques <milamazim@gmail.com>
2026-01-18 07:05:41 -0300

    car form reformulated to use Django native ModelForm

--- cars/views.py
+++ cars/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, redirect
 from cars.models import Car
-from cars.forms import CarForm
+from cars.forms import CarModelForm
 
 def cars_view(request):    
     cars = Car.objects.all().order_by('model') # select * from cars order by model
@@ -14,12 +14,12 @@ def cars_view(request):
 
 def new_car_view(request):
     if request.method == 'POST':
-        new_car_form = CarForm(request.POST, request.FILES) # recebe os dados que o usuario preencheu
+        new_car_form = CarModelForm(request.POST, request.FILES) # recebe os dados que o usuario preencheu
 
         if new_car_form.is_valid(): # valida o form antes de gravar
             new_car_form.save()
             return redirect('cars_list')        
         
     else:        
-        new_car_form = CarForm() # form vazio   
+        new_car_form = CarModelForm() # form vazio   
         return render(request, 'new_car.html', {'new_car_form': new_car_form})
