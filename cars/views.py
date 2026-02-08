e3a7290812de0ba89aa302d1d05578a96cfe85d0
Camila Marques <milamazim@gmail.com>
2026-02-08 10:55:42 -0300

    refactoring function-based views into class-based views for app cars

--- cars/views.py
+++ cars/views.py
@@ -1,26 +1,33 @@
 from django.shortcuts import render, redirect
 from cars.models import Car
 from cars.forms import CarModelForm
-
-def cars_view(request):    
-    cars = Car.objects.all().order_by('model') # select * from cars order by model
-    search = request.GET.get('search') # filtro da busca do usuario
-
-    if search:
-        cars = Car.objects.filter(model__icontains=search).order_by('model')
-
-    return render(request, 'cars.html', {'cars':cars})
+from django.views import View
 
 
-def new_car_view(request):
-    if request.method == 'POST':
+class CarsView(View):
+    
+    def get(self, request):
+        cars = Car.objects.all().order_by('model') # select * from cars order by model
+        search = request.GET.get('search') # filtro da busca do usuario
+
+        if search:
+            cars = Car.objects.filter(model__icontains=search).order_by('model')
+
+        return render(request, 'cars.html', {'cars':cars})
+
+
+class NewCarView(View):
+
+    def get(self, request):
+        new_car_form = CarModelForm() # form vazio
+        return render(request, 'new_car.html', {'new_car_form': new_car_form})   
+    
+    def post(self, request):
         new_car_form = CarModelForm(request.POST, request.FILES) # recebe os dados que o usuario preencheu
 
         if new_car_form.is_valid(): # valida o form antes de gravar
             new_car_form.save()
             return redirect('cars_list')
         
-    else:        
-        new_car_form = CarModelForm() # form vazio   
-    
-    return render(request, 'new_car.html', {'new_car_form': new_car_form})
+        return render(request, 'new_car.html', {'new_car_form': new_car_form})
+
