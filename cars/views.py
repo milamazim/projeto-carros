0e808d22c2d9ceeb470f59620a4fd4732a928930
Camila Marques <milamazim@gmail.com>
2026-02-16 07:55:05 -0300

    adding CarDetailView

--- cars/views.py
+++ cars/views.py
@@ -1,6 +1,6 @@
 from cars.models import Car
 from cars.forms import CarModelForm
-from django.views.generic import ListView, CreateView
+from django.views.generic import ListView, CreateView, DetailView
 
 
 class CarsListView(ListView):
@@ -22,4 +22,9 @@ class NewCarCreateView(CreateView):
     form_class = CarModelForm
     template_name = 'new_car.html'
     success_url = '/cars/' # url para redirecionar
+
+
+class CarDetailView(DetailView):
+    model = Car
+    template_name = 'car_detail.html'    
 
