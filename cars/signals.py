90e88d533106908ea9c8ff2cff0995111fb928bf
Camila Marques <milamazim@gmail.com>
2026-03-01 18:57:10 -0300

    bio description with openai api integration

--- cars/signals.py
+++ cars/signals.py
@@ -1,7 +1,8 @@
-from django.db.models.signals import post_save, post_delete
+from django.db.models.signals import pre_save, post_save, post_delete
 from django.db.models import Sum
 from django.dispatch import receiver
 from cars.models import Car, CarInventory
+from openai_api.client import get_car_ai_bio
 
 
 def car_inventory_update():
@@ -14,6 +15,15 @@ def car_inventory_update():
         cars_count = cars_count,
         cars_value = cars_value
     ) # grava no BD
+
+
+@receiver(pre_save, sender=Car)
+def car_pre_save(sender, instance, **kwargs):
+    if not instance.bio:
+        ai_bio = get_car_ai_bio(
+            instance.model, instance.brand, instance.model_year
+        )
+        instance.bio = ai_bio
 
 
 @receiver(post_save, sender=Car)
